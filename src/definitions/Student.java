0d87e27b3373ddccc077ccc583a984a9ca03e437
divyanshgarg01 <divyansh.garg_cs19@gla.ac.in>
2020-08-30 09:53:36 +0530

    add fields to the class

---
+++ src/definitions/Student.java
@@ -0,0 +1,14 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 21/08/20
+ *  Time: 3:49 PM
+ *  File Name : Student.java
+ * */
+package definitions;
+
+public class Student {
+
+    private String studentName;
+    private long rollNumber;
+    private int numberOfBooksIssued;
+}
